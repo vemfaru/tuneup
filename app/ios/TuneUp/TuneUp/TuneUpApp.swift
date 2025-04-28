92482e317b1cc6ad27fb3e431e894c9c25ac4dc8
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2025-04-28 07:55:13 -0300

    chore: bootstrap ios app

---
+++ app/ios/TuneUp/TuneUp/TuneUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneUpApp.swift
+//  TuneUp
+//
+//  Created by Rodrigo Sicarelli on 28/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TuneUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
